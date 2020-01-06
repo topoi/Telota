c585259ea2c441f7fb71a619e0e2cfa7b2056370
topoi <gordon-fischer@gmx.de>
2020-01-06 14:00:01 +0100

    new version notebook

--- JupyterNotebooks/ediarum_basic.py
+++ JupyterNotebooks/ediarum_basic.py
@@ -6,6 +6,8 @@ import ipywidgets as widgets
 from ipywidgets import interact, interact_manual
 import numpy as np
 import qgrid
+import pandas, io
+from pivottablejs import pivot_ui
 
 selection={}
 def GetActionTable(project="", item="id", SearchString=""):
@@ -45,3 +47,120 @@ def GUI():
         @interact
         def show_articles_more_than1(column=selection[field]):
             return GetActionTable(project="project1", item=field,SearchString=column)
+        
+def LoadBaseFramework(data=""):
+    item="id"
+    project="goedel"
+    SearchString=""
+    tree = ET.parse(data) 
+    root = tree.getroot()
+    xmlstr = ET.tostring(root, encoding='utf8', method='xml')
+    root= etree.XML(xmlstr)
+
+    regexpNS = "http://exslt.org/regular-expressions"
+    basic="*/*/*/*/*/*/"
+    find_actionArray = etree.XPath(basic+"field[@name='actionDescriptors']/action-array", namespaces={'re':regexpNS})
+
+    df_cols=[]
+    finallist=[]
+    for el in find_actionArray(root):
+
+        for j in range(6):
+            df_cols.append(el[0][j].attrib["name"])
+        for i in range(len(el)):
+
+            selection={}
+            actionmode=[]
+
+            for k in range(len(el[i][7][0])):
+
+                try:
+
+                    #actiondict={}
+                    #actiondict[el[i][7][0][k][0].attrib["name"]]=el[i][7][0][k][0][0].text
+
+
+                    argvalues=[]
+                    for m in range(len(el[i][7][0][k][1][0])):
+                        argvaldict={}
+                        argvaldict["arg type"]=str(el[i][7][0][k][1][0][m][0].text)
+                        argvaldict["arg values"]=str(el[i][7][0][k][1][0][m][1].text)
+                        argvaldict[el[i][7][0][k][0].attrib["name"]]=el[i][7][0][k][0][0].text
+                        argvaldict[el[i][7][0][k][2].attrib["name"]]=el[i][7][0][k][2][0].text
+
+                        for j in range(6):
+                            
+                            argvaldict[el[i][j].attrib["name"]]=el[i][j][0].text
+
+                        argvalues.append(argvaldict)
+
+                except:
+                    pass
+
+                actionmode.append(argvalues)
+            finallist.append(actionmode)
+    df=pd.DataFrame.from_dict(finallist[0][0])
+    for i in range(len(finallist)):
+        for j in range(len(finallist[i])):
+            df=df.append(pd.DataFrame.from_dict(finallist[i][j]), ignore_index=True)
+
+    df = df.drop(['largeIconPath', 'smallIconPath','accessKey'], axis=1)
+    df=df.set_index(['id', 'description','name', 'xpathCondition', 'operationID'])
+    
+    qgrid.widget = qgrid.show_grid(df, show_toolbar=True)
+    return qgrid.widget, df
+
+def LoadExtendedFramework(data):
+    tree = ET.parse(data) 
+    root = tree.getroot()
+    xmlstr = ET.tostring(root, encoding='utf8', method='xml')
+    root= etree.XML(xmlstr)
+
+    regexpNS = "http://exslt.org/regular-expressions"
+    basic="*/*/*/*/*/*/"
+    find_actionArray = etree.XPath(basic+"field[@name='patchList']/list/poPatch/field[@name='value']/action", namespaces={'re':regexpNS})
+    df_cols=[]
+    finallist=[]
+    for el in find_actionArray(root):
+
+        for j in range(6):
+            df_cols.append(el[j].attrib["name"])
+
+        for i in range(len(el)):
+
+            selection={}
+            actionmode=[]
+
+            for k in range(len(el[i])):
+                #try:
+
+                argvalues=[]
+                #for m in range(len(el[i][7][0][k][1][0])):
+                for m in range(len(el[i][k])):
+
+                    argvaldict={}
+                    argvaldict["arg type"]=el[i][k][m][1][0][0][0].text
+                    argvaldict["arg values"]=str(el[i][k][m][1][0][0][1].text)
+
+                    argvaldict[el[i][k][m][0].attrib["name"]]=el[i][k][m][0][0].text
+                    argvaldict[el[i][k][m][2].attrib["name"]]=el[i][k][m][2][0].text
+
+                    for j in range(6):
+                        argvaldict[el[j].attrib["name"]]=el[j][0].text
+
+                    argvalues.append(argvaldict)
+                #except:
+                 #   pass
+
+                actionmode.append(argvalues)
+            finallist.append(actionmode)
+    df=pd.DataFrame.from_dict(finallist[0][0])
+    for i in range(len(finallist)):
+        for j in range(len(finallist[i])):
+            df=df.append(pd.DataFrame.from_dict(finallist[i][j]), ignore_index=True)
+
+    df = df.drop(['largeIconPath', 'smallIconPath','accessKey'], axis=1)
+    df=df.set_index(['id', 'description','name', 'xpathCondition', 'operationID'])
+
+    qgrid.widget = qgrid.show_grid(df, show_toolbar=True)
+    return qgrid.widget
